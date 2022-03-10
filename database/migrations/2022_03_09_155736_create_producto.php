9052699a9d32a44fae8bc1e068f2ec684227c38b
Santiago Anacona <davsanacona@misena.edu.co>
2022-03-10 11:21:05 -0500

    v1 del inventario

---
+++ database/migrations/2022_03_09_155736_create_producto.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('producto', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('id_presentacion')->nullable();
+            $table->foreign('id_presentacion')->references('id')->on('presentacion')->onUpdate('cascade')->onDelete('cascade');
+            $table->unsignedBigInteger('id_proveedor')->nullable();
+            $table->foreign('id_proveedor')->references('id')->on('proveedor')->onUpdate('cascade')->onDelete('cascade');
+            $table->unsignedBigInteger('id_marca')->nullable();
+            $table->foreign('id_marca')->references('id')->on('marca')->onUpdate('cascade')->onDelete('cascade');
+            $table->string('descripcion');
+            $table->integer('precio');
+            $table->string('stock');
+            $table->integer('iva');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('producto');
+    }
+};
